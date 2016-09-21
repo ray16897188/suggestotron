6df2fe9aba488e3e0f782f0922fd1e5b5b5c389a
Ray Gao <atlantis.ray@gmail.com>
2016-09-21 21:52:57 +0800

    downcount

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
   resources :topics do
   	member do
   		post 'upvote'
+  		post 'downvote'
+  		# delete 'delete'
   	end
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
